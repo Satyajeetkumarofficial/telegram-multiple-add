00a504a0f2ca8a8bbd67fdd575ae5bf713f0c535
Satyajeetkumarofficial <152394869+Satyajeetkumarofficial@users.noreply.github.com>
2025-03-11 22:33:24 +0530

    Update and rename config.example.py to config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+api_id = 23971012
+api_hash = "2efec8e3811a1d3a27ffbf5c12f03110"
+phone = "+919835147684"
+group_link = "https://t.me/MoviesSearchPro"
+admins_file_path = os.path.join("TGKing999", "admins.txt")
+members_file_path = os.path.join("Hindi_Movie_Hub_HD_Group", "members.txt")
